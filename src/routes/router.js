b78c46e9e856cdeddc3434ea85032aeb4ddad26c
Guilherme <guilhermepa3@gmail.com>
2022-02-10 16:59:00 -0300

    fix: add marketRoute on router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authRouter } from "./authRoute.js";
 import { loginRouter } from "./authRoute.js";
+import { marketRouter } from "./marketRoute.js";
 const router = Router();
 
 router.use(authRouter);
 router.use(loginRouter);
+router.use(marketRouter);
 
 export {
     router
