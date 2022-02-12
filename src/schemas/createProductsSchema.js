6049693e94915ee39b35b441349f2e52a3cd4852
Guilherme <guilhermepa3@gmail.com>
2022-02-12 19:43:20 -0300

    resolve bug on createProductsSchema

--- src/schemas/createProductsSchema.js
+++ src/schemas/createProductsSchema.js
@@ -4,7 +4,7 @@ const createProduct = joi.object({
     name: joi.string().min(3).max(40).required(),
     price: joi.number().min(1).required(),
     stock: joi.number().min(0).required(),
-    images: joi.array().min(1).items(joi.string().pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg))/i)),
+    images: joi.string().pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg))/),
 });
 
 export {
