bc8211b201bb4fe1ff869e681dc67fbd9cf0dbeb
Guilherme <guilhermepa3@gmail.com>
2022-02-12 19:47:00 -0300

    fix: bug on card image

--- src/schemas/createProductsSchema.js
+++ src/schemas/createProductsSchema.js
@@ -4,7 +4,7 @@ const createProduct = joi.object({
     name: joi.string().min(3).max(40).required(),
     price: joi.number().min(1).required(),
     stock: joi.number().min(0).required(),
-    images: joi.string().pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg))/),
+    image: joi.string().pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg))/),
 });
 
 export {
