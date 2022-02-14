a2a6f838466fbe410d3d7a22481fd1a92b144691
LucasVz <lucastadeuvaz@gmail.com>
2022-02-14 11:00:24 -0300

    fix db cart

--- src/routes/marketRoute.js
+++ src/routes/marketRoute.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { getProducts, postProducts, postCart } from "../controllers/marketController.js";
+import { getProducts, postProducts, postCart, getCart } from "../controllers/marketController.js";
+import { validateCreateProductMiddleware } from "../middlewares/validateCreateProductMiddleware.js"
+import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const marketRouter = Router();
 
 marketRouter.get("/products", getProducts);
-marketRouter.post("/products", postProducts);
-marketCart.post("/cart", postCart)
+marketRouter.post("/products",validateCreateProductMiddleware, postProducts);
+marketRouter.post("/cart",validateTokenMiddleware, postCart);
+marketRouter.get("/cart", getCart)
 
 export {
     marketRouter
